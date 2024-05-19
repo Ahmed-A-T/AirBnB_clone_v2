dc0f9138971b639d9c48bdfe7c8ec04a790a915d
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-05-19 22:22:25 +0300

    Fix the file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,17 +1,19 @@
--- Script that prepares a MySQL server for the project.
--- A database hbnb_dev_db
--- A new use hbnb_dev in localhost.
--- The password hbnb_dev_pwd.
--- The database hbnb_dev_db
+-- Creates a MySQL server with:
+--   Database hbnb_test_db.
+--   User hbnb_test with password hbnb_test_pwd in localhost.
+--   Grants all privileges for hbnb_test on hbnb_test_db.
+--   Grants SELECT privilege for hbnb_test on performance_schema.
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER
-    IF NOT EXISTS `hbnb_test`@`localhost`
+    IF NOT EXISTS 'hbnb_test'@'localhost'
     IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
    ON `hbnb_test_db`.*
-   TO `hbnb_test`@`localhost`;
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT
    ON `performance_schema`.*
-   TO `hbnb_test`@`localhost`;
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
