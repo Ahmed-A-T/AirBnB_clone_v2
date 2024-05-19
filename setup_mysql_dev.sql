be03e5d215084876f6ddb5242bb928cfc981ebdf
Ahmed Abdelhafez <ahmedhafez.ta@gmail.com>
2024-05-19 22:04:29 +0300

    Modifying the file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,11 @@
--- Script that prepares a MySQL server for the project.
--- A database hbnb_dev_db
--- A new use hbnb_dev in localhost.
--- The password hbnb_dev_pwd.
--- The database hbnb_dev_db
-
+-- Check if the database exists, create it if not
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS `hbnb_dev`@`localhost`
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO `hbnb_dev`@`localhost`;
-GRANT SELECT
-   ON `performance_schema`.*
-   TO `hbnb_dev`@`localhost`;
-FLUSH PRIVILEGES;
+
+-- Create or update the user 'hbnb_dev' with the password 'hbnb_dev_pwd'
+CREATE OR REPLACE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the 'hbnb_dev_db' database to 'hbnb_dev'
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the 'performance_schema' database to 'hbnb_dev'
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
